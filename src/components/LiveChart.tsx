e8a28e4b8243e337ab01e6e016dbadea3aefbd34
Daniel Constantin <hello@danielconstantin.net>
2023-05-09 14:15:26 +0300

    Full tradingview  attempt

---
+++ src/components/LiveChart.tsx
@@ -0,0 +1,23 @@
+// import React, { useEffect } from "react"
+// import * as TradingView from "charting_library"
+// console.log("📜 LOG > TradingView", TradingView);
+
+// export function LiveChart(props) {
+//   useEffect(() => {
+    
+//   console.log("📜 LOG > TradingView", TradingView.version());
+
+//     new TradingView.widget({
+//       container_id: "chartContainer",
+//       datafeed: "https://demo-feed-data.tradingview.com"
+//     })
+//   }, [])
+
+//   return (
+//     <>
+// <div id="chartContainer"></div>
+//     {/* {TradingView.version()} */}
+//       hey222
+//     </>
+//   )
+// }
