f15f9cfb1b5cee867ab9feea8aceb8cdebc91c49
Daniel Constantin <hello@danielconstantin.net>
2024-01-08 17:42:28 +0200

    Make FileImportTableRow responsive

--- src/pages/ImportDataPage/file-imports/FileImportTableRow.tsx
+++ src/pages/ImportDataPage/file-imports/FileImportTableRow.tsx
@@ -25,14 +25,7 @@ import { TableRowComponentProps } from "src/utils/table-utils"
 import { clancy } from "src/workers/remotes"
 
 export function FileImportTableRow(props: TableRowComponentProps<FileImport>) {
-  const {
-    row,
-    relativeTime,
-    headCells: _headCells,
-    isMobile: _isMobile,
-    isTablet: _isTablet,
-    ...rest
-  } = props
+  const { row, relativeTime, headCells: _headCells, isMobile: _isMobile, isTablet, ...rest } = props
   const { name, meta, timestamp, lastModified, size } = row
   const integration = meta?.integration
 
@@ -61,17 +54,107 @@ export function FileImportTableRow(props: TableRowComponentProps<FileImport>) {
     })
   }
 
-  // const isTablet = useMediaQuery("(max-width: 899px)")
-
-  // if (isTablet) {
-  //   return (
-  //     <TableRow hover {...rest}>
-  //       <TableCell sx={{ width: "100%" }}>
-  //         <Stack direction="row" justifyContent="space-between" alignItems="center"></Stack>
-  //       </TableCell>
-  //     </TableRow>
-  //   )
-  // }
+  if (isTablet) {
+    return (
+      <TableRow hover {...rest}>
+        <TableCell sx={{ width: "100%" }}>
+          <Stack direction="row" justifyContent="space-between" alignItems="center">
+            <Stack gap={0.5}>
+              <Stack direction="row" gap={1} alignItems="center" component="div">
+                {integration ? (
+                  <IntegrationAvatar
+                    size="small"
+                    src={integrationMap[integration]?.image}
+                    alt={INTEGRATIONS[integration]}
+                  />
+                ) : (
+                  <Skeleton></Skeleton>
+                )}
+                <Tooltip title={name}>
+                  <Truncate>{name}</Truncate>
+                </Tooltip>
+              </Stack>
+              <Stack
+                direction="row"
+                gap={1}
+                alignItems="center"
+                component="div"
+                sx={{ color: "text.secondary", fontSize: "0.75rem" }}
+              >
+                <TimestampBlock timestamp={timestamp} relative={relativeTime} />
+                {" • "}
+                <Tooltip
+                  title={
+                    <Stack>
+                      <span>{formatFileSize(size, true)}</span>
+                      <i className="secondary">
+                        <span>{formatNumber(size)} Bytes</span>
+                      </i>
+                    </Stack>
+                  }
+                >
+                  <span>{formatFileSize(size)}</span>
+                </Tooltip>
+              </Stack>
+            </Stack>
+            <Stack gap={0.5} alignItems={"flex-end"} sx={{ fontSize: "0.75rem" }}>
+              {!meta ? (
+                <Skeleton></Skeleton>
+              ) : (
+                <>
+                  {meta.logs === meta.rows ? (
+                    <span>{formatNumber(meta.logs)} audit logs</span>
+                  ) : (
+                    <Tooltip
+                      title={`${formatNumber(meta.logs)} audit logs extracted from ${formatNumber(
+                        meta.rows
+                      )} document entries`}
+                    >
+                      <span>{formatNumber(meta.logs)}</span>
+                    </Tooltip>
+                  )}
+                </>
+              )}
+              {!meta ? (
+                <Skeleton></Skeleton>
+              ) : (
+                <>
+                  <Tooltip
+                    title={`${formatNumber(
+                      meta.transactions
+                    )} transactions extracted from ${formatNumber(meta.rows)} document entries`}
+                  >
+                    <span>{formatNumber(meta.transactions)} txns</span>
+                  </Tooltip>
+                </>
+              )}
+            </Stack>
+          </Stack>
+        </TableCell>
+        <TableCell sx={{ maxWidth: 40, minWidth: 40, width: 40 }}>
+          <Tooltip
+            title={loading ? "Removing..." : "Remove file import (including its audit logs)"}
+          >
+            <span>
+              <IconButton
+                size="small"
+                color="secondary"
+                sx={{ height: 28, marginLeft: -1 }}
+                onClick={handleRemove}
+                disabled={loading}
+              >
+                {loading ? (
+                  <CircularProgress size={14} color="inherit" />
+                ) : (
+                  <HighlightOffRounded fontSize="inherit" />
+                )}
+              </IconButton>
+            </span>
+          </Tooltip>
+        </TableCell>
+      </TableRow>
+    )
+  }
 
   return (
     <TableRow hover {...rest}>
